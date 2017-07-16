33a25ad41d22a9b0245c1f35471f00ccf1c070e1
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-07-16 18:59:39 +0900

    Add LoadingContainer

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <loading-conatiner :loading="isLoading">
+      <img src="./assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <h2>Essential Links</h2>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+        <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      </ul>
+    </loading-conatiner>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import LoadingConatiner from './LoadingConatiner.vue'
 
 @Component({
-  name: 'app',
+  components: {
+    LoadingConatiner,
+  }
 })
 export default class App extends Vue {
   msg = 'Welcome to Your Vue.js App'
+  isLoading = true
+
+  mounted() {
+    setTimeout(() => { this.isLoading = false }, 5000)
+  }
 }
 </script>
 
---
+++ src/LoadingConatiner.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="loading-container">
+        <transition name="fade" key="loading">
+            <div v-if="loading" class="loading">
+                <pulse-loader></pulse-loader>
+            </div>
+            <div v-else key="loaded" class="loaded">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+
+@Component({
+    props: {
+        loading: Boolean
+    },
+    components: { PulseLoader }
+})
+export default class LoadingConatiner extends Vue {
+}
+</script>
+
+<style>
+.loading-container {
+    position: relative;
+}
+
+.loading-container .loading {
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    right: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
+}
+</style>
